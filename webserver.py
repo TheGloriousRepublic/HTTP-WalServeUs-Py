88715cebc3d7d3b2fa31bdabc6ed98a68eaf196f
hppavilion1 <nathantronnes@gmail.com>
2015-04-26 22:23:37 -0800

    CSS handling, Graceful Shutdown (Sort of), etc.
    Any edits to .log files should be ignored

--- webserver.py
+++ webserver.py
@@ -1,21 +1,26 @@
-import Tkinter, BaseHTTPServer, socket, os, sys, datetime
+import Tkinter, BaseHTTPServer, socket, os, sys, datetime, atexit
 
 settings = {}
 
-def loadConfig():
-    for root, dirs, files in os.walk('config/'):
+def loadConfig(): #Open configuration files and save their options to settings
+    for root, dirs, files in os.walk('config/settings/'): #Open all files in config directory
         for f in files:
-            if f.endswith(".cfg"):
-                for x in open('config/'+f).read().split('\n'):
-                    settings[x.split('=')[0]]=x.split('=')[1]
+            if f.endswith('.cfg'):
+                for x in open('config/'+f).read().split('\n'): #Separate lines in file and iterate
+                    settings[x.split('=')[0]]=x.split('=')[1] #Set the option before the equal sign in the config file line to the value in the settings dict
+    for root, dirs, files in os.walk('config/settings/'): #Open all files in config directory
+        for f in files:
+            if f.endswith('.cfg'):
+                for x in open('config/'+f).read().split('\n'): #Separate lines in file and iterate
+                    settings[x.split('=')[0]]=x.split('=')[1] #Set the option before the equal sign in the config file line to the value in the settings dict
 
-def log(dat):
+def log(dat): #Print to console and save to log
     o='['+str(datetime.datetime.now())+'] '+str(dat)
-    b=open(settings['lgdir']+'/server.log').read()
-    open(settings['lgdir']+'/server.log', 'w').write(b+o+'\n')
+    b=open(settings['lgdir']+'/server.log').read() #Retrieve current log
+    open(settings['lgdir']+'/server.log', 'w').write(b+o+'\n') #Write a concatenation of old log and new log to log
     print(o)
 
-class webServer(BaseHTTPServer.BaseHTTPRequestHandler):
+class webServer(BaseHTTPServer.BaseHTTPRequestHandler): #Main handler class
 
     def log_message(*args):
         pass
@@ -37,37 +42,50 @@ class webServer(BaseHTTPServer.BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type','text/html')
             self.end_headers()
+        elif self.path.endswith('.css'):
+            self.send_response(200)
+            self.send_header('Content-type','text/css')
+            self.end_headers()
 
     def do_GET(self):
         if self.path.endswith('/'):
             self.path=self.path[:-1]
-        self.do_HEAD()
-        if os.path.isfile(os.curdir+os.sep+self.path):
-            if self.path.endswith('.html'):
-                self.wfile.write(open(os.curdir+os.sep+self.path).read())
+        if os.path.isfile(settings['pgdir']+'/'+self.path):
+            if self.path.endswith('.html') or self.path.endswith('.css'):
+                self.do_HEAD()
+                self.wfile.write(open(settings['pgdir']+'/'+self.path).read())
+            else:
+                self.do_HEAD()
+                self.wfile.write(open(settings['erdir']+'/blocked.html').read())
         else:
-            self.wfile.write(open(os.curdir+os.sep+'errors/404.html').read())
+            self.wfile.write(open(settings['erdir']+'/404.html').read())
     def do_KILL(self):
         sys.exit()
 
+def gracefulShutdown():
+    log('Server stops')
+    s.server_close
+    c='\n'.join(connected)
+    f=open(settings['lgdir']+'/connected.log', 'w')
+    f.write(c)
+    f=open(settings['lgdir']+'/visitorcount.log', 'w')
+    f.write(str(individualvisitors))
+    f.close()
 
 def serve():
-    global connected
+    global connected, s
     s = BaseHTTPServer.HTTPServer(('', 80), webServer)
     log('Server starts')
     try:
         s.serve_forever()
-    except KeyboardInterrupt:
-        s.server_close
-        c='\n'.join(connected)
-        open('logs/connected.log', 'w').write(c)
-            
-        log('Server stops')
-
+    except:
+        pass
 loadConfig()
 
-connected=open(settings['lgdir']+os.sep+'connected.log').read().split('\n')
-visitors=int(open(settings['lgdir']+os.sep+'/visitorcount.log').read())
-individualvisitors=len(connected)
+connected=list(open(settings['lgdir']+'/connected.log').read().split('\n')) #Retrieve list of connected IPs
+visitors=int(open(settings['lgdir']+'/visitorcount.log').read()) #Retrieve number of connectors
+individualvisitors=len(connected) #Get number of unique connectors
 
-serve()
+atexit.register(gracefulShutdown) #Record logs at shutdown
+if __name__ == '__main__':
+    serve()
