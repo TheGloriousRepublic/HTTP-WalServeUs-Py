d7c28b68a45437151301554b0ca682d79ec45f5c
hppavilion1 <nathantronnes@gmail.com>
2015-04-29 12:05:34 -0800

    Fallback files on 404/403, fixed binary file header
    It was sending two headers on error.

--- WalServeUs.py
+++ WalServeUs.py
@@ -11,18 +11,20 @@ def loadConfig(): #Open configuration files and save their options to settings
         for f in files:
             if f.endswith('.cfg'):
                 for x in open('config/settings/'+f).read().split('\n'): #Separate lines in file and iterate
-                    settings[x.split('=')[0]]=x.split('=')[1] #Set the option before the equal sign in the config file line to the value in the settings dict
+                    if not x[0]=='#':
+                        settings[x.split('=')[0]]=x.split('=')[1] #Set the option before the equal sign in the config file line to the value in the settings dict
                     
     for root, dirs, files in os.walk('config/rewriter/'):
         for f in files:
             if f.endswith('.cfg'):
                 for x in open('config/rewriter/'+f).read().split('\n'):
-                    rw[x.split('=')[0]]=x.split('=')[1]
+                    if not x[0]=='#':
+                        rw[x.split('=')[0]]=x.split('=')[1]
 
 def log(dat): #Print to console and save to log
-    o='['+str(datetime.datetime.now())+'] '+str(dat)
+    o='['+str(datetime.datetime.now())+'] '+str(dat)+'\n'
     b=open(settings['lgdir']+'/server.log').read() #Retrieve current log
-    open(settings['lgdir']+'/server.log', 'w').write(b+o+'\n') #Write a concatenation of old log and new log to log
+    open(settings['lgdir']+'/server.log', 'w').write(b+o+'\n\n') #Write a concatenation of old log and new log to log
     print(o)
 
 class webServer(BaseHTTPServer.BaseHTTPRequestHandler): #Main handler class
@@ -66,16 +68,18 @@ class webServer(BaseHTTPServer.BaseHTTPRequestHandler): #Main handler class
         ext=p.split('.')[-1]
         self.sendHeader()
         if os.path.isfile(settings['pgdir']+'/'+p):
-            #print mimetypes.guess_type(p)[0].split('/')[0]
             if ext in settings['pgexr'].split('|') or ('*' in settings['pgexr'].split('|') and mimetypes.guess_type(p)[0].split('/')[0] in serveAsPlaintext):
                 self.wfile.write(open(settings['pgdir']+'/'+p).read())
-            elif ext in settings['pgexb'].split('|') or ('*' in settings['pgexb'].split('|') and not mimetypes.guess_type(p)[0].split('/')[0] in serveAsPlaintext):
-                self.sendHeader()
+            elif ext in settings['pgexb'].split('|') or ('*' in settings['pgexb'].split('|') and not (mimetypes.guess_type(p)[0].split('/')[0] in serveAsPlaintext)):
                 self.wfile.write(open(settings['pgdir']+'/'+p,'rb').read())
-            else:
+            elif os.path.isfile(settings['erdir']+'/403.html'):
                 self.wfile.write(open(settings['erdir']+'/403.html').read())
-        else:
+            else:
+                self.wfile.write('<h1>Error 403</h1><h2>You are forbidden to access this file on this server</h2>Furthermore, no 403.html file was found in the local server\'s error directory')
+        elif os.path.isfile(settings['erdir']+'/404.html'):
             self.wfile.write(open(settings['erdir']+'/404.html').read())
+        else:
+            self.wfile.write('<h1>Error 404</h1><h2>File not found</h2>Furthermore, no 404.html file was found in the local server\'s error directory')
 
     def do_POST(self):
         p=self.getPath()
