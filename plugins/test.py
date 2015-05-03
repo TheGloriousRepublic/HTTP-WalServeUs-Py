211880812f976d0192eab7b4576f506ad2fe1fd9
hppavilion1 <nathantronnes@gmail.com>
2015-05-03 12:57:14 -0800

    Plugin Support Complete!
    Yaaaaaaaaay

--- plugins/test.py
+++
@@ -1,5 +0,0 @@
-def happybirthday(name):
-    return('Happy birthday to you\nHappy birthday to you\nHappy birthday dear '+name+'\nHappy birthday to you')
-
-def main(i):
-    return(happybirtday('Nathan')+i.replace('a','b'))
